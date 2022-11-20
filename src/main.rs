8288ccbf0449fe3531c8a5322c409ca3c720d3a9
James Waples <james@wapl.es>
2022-11-20 23:31:01 +0000

    Add encoder and readout

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,39 @@
 use display_interface_spi::SPIInterfaceNoCS;
-use embedded_graphics::pixelcolor::Rgb565;
-use embedded_graphics::prelude::*;
-use embedded_graphics::primitives::PrimitiveStyleBuilder;
-use embedded_graphics::primitives::Rectangle;
-use esp_idf_hal::delay::Ets;
-use esp_idf_hal::gpio::*;
-use esp_idf_hal::peripherals::Peripherals;
-use esp_idf_hal::spi::*;
-use esp_idf_hal::units::FromValueType;
-use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
-use std::thread;
-use std::time::Duration;
-
+use embedded_graphics::{
+    mono_font::{ascii::FONT_7X13, MonoTextStyle, MonoTextStyleBuilder},
+    pixelcolor::Rgb565,
+    prelude::*,
+    primitives::{PrimitiveStyleBuilder, Rectangle},
+    text::Text,
+};
+use esp_idf_hal::{
+    delay::Ets,
+    gpio::{self, *},
+    peripherals::Peripherals,
+    spi::*,
+    units::FromValueType,
+};
+use esp_idf_sys as _;
 use mipidsi::{Builder, Orientation};
+use rotary_encoder_embedded::{Direction, RotaryEncoder};
+use std::{
+    sync::{
+        atomic::{AtomicBool, AtomicI32, Ordering},
+        Arc,
+    },
+    thread,
+    time::Duration,
+};
 
 fn main() -> anyhow::Result<()> {
     let peripherals = Peripherals::take().unwrap();
     let spi = peripherals.spi2;
+
+    let rotary_a = PinDriver::input(peripherals.pins.gpio1)?;
+    let rotary_b = PinDriver::input(peripherals.pins.gpio2)?;
+    let button = PinDriver::input(peripherals.pins.gpio3)?;
+
+    let mut rotary_encoder = RotaryEncoder::new(rotary_a, rotary_b).into_standard_mode();
 
     let rst = PinDriver::output(peripherals.pins.gpio8)?;
     let dc = PinDriver::output(peripherals.pins.gpio9)?;
@@ -25,9 +42,6 @@ fn main() -> anyhow::Result<()> {
     let sclk = peripherals.pins.gpio6;
     let sda = peripherals.pins.gpio7;
 
-    let sdi = peripherals.pins.gpio1;
-    let cs = peripherals.pins.gpio2;
-
     let mut delay = Ets;
 
     // configuring the spi interface, note that in order for the ST7789 to work, the data_mode needs to be set to MODE_3
@@ -35,8 +49,17 @@ fn main() -> anyhow::Result<()> {
         .baudrate(26.MHz().into())
         .data_mode(embedded_hal::spi::MODE_3);
 
-    let device =
-        SpiDeviceDriver::new_single(spi, sclk, sda, Some(sdi), Dma::Disabled, Some(cs), &config)?;
+    let device = SpiDeviceDriver::new_single(
+        spi,
+        sclk,
+        sda,
+        // Some(sdi),
+        Option::<gpio::Gpio1>::None,
+        Dma::Auto(1024),
+        // Some(cs),
+        Option::<gpio::Gpio2>::None,
+        &config,
+    )?;
 
     // display interface abstraction from SPI and DC
     let di = SPIInterfaceNoCS::new(device, dc);
@@ -60,20 +83,75 @@ fn main() -> anyhow::Result<()> {
     display.clear(Rgb565::BLACK).unwrap();
     // ferris.draw(&mut display).unwrap();
 
-    Rectangle::new(Point::new(10, 10), Size::new(40, 50))
-        .into_styled(
-            PrimitiveStyleBuilder::new()
-                .stroke_width(3)
-                .stroke_color(Rgb565::RED)
-                .fill_color(Rgb565::GREEN)
-                .build(),
+    // Rectangle::new(Point::new(10, 10), Size::new(40, 50))
+    //     .into_styled(
+    //         PrimitiveStyleBuilder::new()
+    //             .stroke_width(3)
+    //             .stroke_color(Rgb565::RED)
+    //             .fill_color(Rgb565::GREEN)
+    //             .build(),
+    //     )
+    //     .draw(&mut display)
+    //     .unwrap();
+
+    // println!("Image printed!");
+
+    let mut count = Arc::new(AtomicI32::new(0));
+    let mut button_state = Arc::new(AtomicBool::new(false));
+
+    let count_writer = count.clone();
+    let button_state_writer = button_state.clone();
+
+    thread::spawn(move || {
+        loop {
+            rotary_encoder.update();
+
+            match rotary_encoder.direction() {
+                Direction::Clockwise => {
+                    count_writer.fetch_sub(1, Ordering::Relaxed);
+                }
+                Direction::Anticlockwise => {
+                    count_writer.fetch_add(1, Ordering::Relaxed);
+                }
+                Direction::None => {
+                    // Do nothing
+                }
+            }
+
+            // TODO: `debouncr`
+            if button.is_high() {
+                button_state_writer.store(false, Ordering::Relaxed);
+            } else {
+                button_state_writer.store(true, Ordering::Relaxed);
+            }
+
+            thread::sleep(Duration::from_millis(10));
+        }
+    });
+
+    let style = MonoTextStyleBuilder::new()
+        .font(&FONT_7X13)
+        .background_color(Rgb565::BLACK)
+        .text_color(Rgb565::WHITE)
+        .build();
+
+    loop {
+        Text::new(
+            &format!(
+                "Count: {}\nButton: {}",
+                count.load(Ordering::Relaxed),
+                if button_state.load(Ordering::Relaxed) {
+                    "1"
+                } else {
+                    "0"
+                },
+            ),
+            Point::new(20, 20),
+            style,
         )
         .draw(&mut display)
         .unwrap();
 
-    println!("Image printed!");
-
-    loop {
-        thread::sleep(Duration::from_millis(1000));
+        thread::sleep(Duration::from_millis(100));
     }
 }
